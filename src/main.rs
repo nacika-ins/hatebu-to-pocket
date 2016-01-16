b21819db49e5b61262f0a7b92c8e5a8ca399ee0e
nacika <nacika@nacika-no-MacBook.local>
2016-01-16 15:05:19 +0900

    (http) callback

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,34 @@ use iron::status;
 fn main() {
     let port = option_env!("PORT").unwrap_or("3000");
     let url = format!("localhost:{}", port);
-    Iron::new(|_: &mut Request| Ok(Response::with((status::Ok, "Hello world!"))))
+    Iron::new(|request: &mut Request| Ok(Response::with((status::Ok, callback(request)))))
         .http(&*url)
         .unwrap();
+}
 
 
+struct Link {
+    name: String,
+    url: String,
+    tags: Vec<String>,
+}
+
+
+fn callback(request: &mut Request) -> String {
+
+    let link = parse_link(request);
+
+    "ok".to_owned()
+}
+
+
+fn parse_link(request: &mut Request) -> Link {
+
+    println!("{:?}", request);
+
+    Link {
+        name: "dummy".to_owned(),
+        url: "dummy".to_owned(),
+        tags: vec![],
+    }
 }
