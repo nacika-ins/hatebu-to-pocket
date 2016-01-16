ce1c475c5b8f33a19b10e7cea606a9e88dd85410
nacika <nacika.inscatolare@gmail.com>
2016-01-17 05:14:50 +0900

    (pocket) send bookmark to pocket

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,23 @@ use iron::typemap::Key;
 extern crate bodyparser;
 extern crate persistent;
 
+extern crate router;
+use router::Router;
+
 use persistent::Read as perRead;
+use persistent::Write as perWrite;
 
 use std::fs::File;
 use std::io::prelude::*;
 
 extern crate regex;
 use regex::Regex;
+
+extern crate pocket;
+use pocket::Pocket;
+
+use std::thread::sleep;
+use std::time::Duration;
 
 struct Link {
     url: String,
@@ -31,11 +41,26 @@ impl Key for ApiKey {
 }
 
 
+struct PocketMail;
+impl Key for PocketMail {
+    type Value = String;
+}
+
+struct PocketWrap;
+impl Key for PocketWrap {
+    type Value = Pocket;
+}
+
 extern crate toml;
 
 
 extern crate queryst;
 use queryst::parse;
+
+extern crate url;
+use url::Url;
+use url::percent_encoding::utf8_percent_encode;
+use url::percent_encoding::FORM_URLENCODED_ENCODE_SET;
 
 const MAX_BODY_LENGTH: usize = 1024 * 1024 * 10;
 
@@ -45,30 +70,124 @@ fn main() {
     let mut toml = String::new();
     let _ = f.read_to_string(&mut toml);
     let value = toml::Parser::new(&toml).parse().unwrap();
-    let apikey = value.get("apikey")
+    let apikey = value.get("hatena")
                       .unwrap()
                       .lookup("apikey")
                       .unwrap()
                       .as_str()
                       .unwrap()
                       .to_owned();
+    let consumer_key = value.get("pocket")
+                            .unwrap()
+                            .lookup("consumer_key")
+                            .unwrap()
+                            .as_str()
+                            .unwrap()
+                            .to_owned();
+    let redirect_url = value.get("pocket")
+                            .unwrap()
+                            .lookup("redirect_url")
+                            .unwrap()
+                            .as_str()
+                            .unwrap()
+                            .to_owned();
+    let pocket_mail = value.get("pocket")
+                           .unwrap()
+                           .lookup("mail")
+                           .unwrap()
+                           .as_str()
+                           .unwrap()
+                           .to_owned();
+
+    let access_token = {
+        let mut f = File::open("pocket.toml");
+        if f.is_ok() {
+            let mut f = f.unwrap();
+            let mut toml = String::new();
+            let _ = f.read_to_string(&mut toml);
+            let value = toml::Parser::new(&toml).parse().unwrap();
+            let access_token = value.get("pocket")
+                                    .unwrap()
+                                    .lookup("access_token")
+                                    .unwrap()
+                                    .as_str()
+                                    .unwrap()
+                                    .to_owned();
+            access_token
+        } else {
+            "".to_owned()
+        }
+    };
+    println!("access_token --> {}", access_token);
 
     let port = option_env!("PORT").unwrap_or("3000");
     let url = format!("localhost:{}", port);
 
-    // Server Start
-    // Iron::new(|request: &mut Request| {
-    //     Ok(Response::with((status::Ok, callback(request, apikey.clone()))))
-    // })
-    //     .http(&*url)
-    //     .unwrap();
 
-    let mut chain = Chain::new(callback);
+    // サーバー起動
+    let mut router = Router::new();
+    router.post("/_/hatena_pocket/", callback);
+    router.get("/_/pocket_auth/", auth_pocket);
+    let mut chain = Chain::new(router);
     chain.link_before(perRead::<bodyparser::MaxBodyLength>::one(MAX_BODY_LENGTH));
     chain.link_before(perRead::<ApiKey>::one(apikey));
+    chain.link_before(perRead::<PocketMail>::one(pocket_mail));
+
+
+    // Authorize Pocket
+    if access_token == "" {
+        let mut pocket = Pocket::new(&*consumer_key, None);
+        let url = pocket.get_auth_url().unwrap();
+        let url = url.to_string().replace("rustapi%3Afinishauth",
+                                          &*utf8_percent_encode(&*redirect_url,
+                                                                FORM_URLENCODED_ENCODE_SET));
+        println!("Follow the link to authorize the app: {}", url);
+        chain.link_before(perWrite::<PocketWrap>::one(pocket));
+    } else {
+        let mut pocket = Pocket::new(&*consumer_key, Some(&*access_token));
+        chain.link_before(perWrite::<PocketWrap>::one(pocket));
+    }
+
     Iron::new(chain).http("localhost:3000").unwrap();
 }
 
+
+fn auth_pocket(request: &mut Request) -> IronResult<Response> {
+    let pocket_mail = request.get::<perRead<PocketMail>>().unwrap();
+    println!("pocket_mail --> {}", pocket_mail);
+    let mut mutex = request.get_mut::<perWrite<PocketWrap>>();
+    match mutex {
+        Ok(mutex) => {
+            // let apikey = request.get::<perRead<ApiKey>>().unwrap();
+            // println!("apikey --> {}", apikey);
+            // println!("{:?}", request);
+            // let body = request.get::<bodyparser::Raw>();
+            // println!("{:?}", body);
+
+            let mut pocket = mutex.lock().unwrap();
+            let username = pocket.authorize();
+
+            // Authenticate with the agreement of the e-mail address
+            if username.is_ok() && pocket_mail.to_string() == username.unwrap() {
+                println!("認証に成功しました");
+                // println!("username --> {}", username.unwrap());
+                // println!("access_token --> {}", pocket.access_token().unwrap());
+
+                let data = format!("[pocket]\naccess_token = \"{}\"",
+                                   pocket.access_token().unwrap());
+                let mut f = File::create("pocket.toml").unwrap();
+                f.write_all(&data.into_bytes());
+                drop(f);
+
+            } else {
+                println!("認証に失敗しました");
+            }
+        }
+        Err(_) => {}
+    }
+
+    Ok(Response::with(status::Ok))
+}
 
 fn callback(request: &mut Request) -> IronResult<Response> {
     let apikey = request.get::<perRead<ApiKey>>().unwrap();
@@ -80,7 +199,23 @@ fn callback(request: &mut Request) -> IronResult<Response> {
         println!("--> APIキーが一致しています");
 
         // Send Pocket
-        // FIXME
+        let mut mutex = request.get_mut::<perWrite<PocketWrap>>();
+        match mutex {
+            Ok(mutex) => {
+                let mut pocket = mutex.lock().unwrap();
+                let mut tags = link.tags.clone();
+                tags.push("hatenabookmark".to_owned());
+                let added_item = pocket.add(&*link.url,
+                                            Some(&*link.title),
+                                            Some(&*tags.join(",")),
+                                            None)
+                                       .unwrap();
+                println!("Pocket投稿結果 --> {:?}", added_item);
+            }
+            Err(_) => {
+                println!("Pocketに投稿が失敗しました");
+            }
+        }
 
         // Send Evernote
         // FIXME
